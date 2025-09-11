90ae70b6a4f1ff8ddfba56174d626e25297aa513
FerryCalvin <110297689+FerryCalvin@users.noreply.github.com>
2025-09-11 10:42:47 +0700

    updated product section
    clearing the product section into functionally smh

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -1,9 +1,14 @@
 package productcontroller
 
 import (
+	"golang/entities"
+	"golang/models/categorymodel"
+	"golang/models/productmodel"
 	"html/template"
 	"net/http"
-	"golang/models/productmodel"
+	"time"
+	"strconv"
+
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +26,23 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func Detail(w http.ResponseWriter, r *http.Request) {
+	idString := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		panic(err)
+	}
 
+	product :=productmodel.Detail(id)
+	data := map[string]any{
+		"product": product,
+	}
+
+	temp, err := template.ParseFiles("views/product/detail.html")
+	if err != nil {
+		panic(err)
+	}
+
+	temp.Execute(w, data)
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
@@ -31,19 +52,111 @@ func Add(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}	
 
-		categories := productmodel.GetAll()
+		categories := categorymodel.GetAll()
 		data := map[string]any{
 			"categories": categories,
 		}
 
 		temp.Execute(w, data)
 	}
+
+	if r.Method == "POST" {
+		var product entities.Product
+
+		categoryId, err := strconv.Atoi(r.FormValue("category_id"))
+		if err != nil {
+			panic(err)
+		}
+
+		stock, err := strconv.Atoi(r.FormValue("stock"))
+		if err != nil {
+			panic(err)
+		}
+
+		product.Name = r.FormValue("name")
+		product.Category.Id = uint(categoryId)
+		product.Stock = int64(stock)
+		product.Description = r.FormValue("description")
+		product.CreatedAt = time.Now()
+		product.UpdatedAt = time.Now()
+
+		if ok := productmodel.Create(product); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusTemporaryRedirect)
+			return
+		}
+
+		http.Redirect(w, r, "/products", http.StatusSeeOther)
+	}
+
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/product/edit.html")
+		if err != nil {
+			panic(err)
+		}
 
-}
+		idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+		
+		product := productmodel.Detail(id)
+		categories := categorymodel.GetAll()
+		data := map[string]any{
+			"categories": categories,
+			"product": product,
+		}
+		temp.Execute(w, data)
+	}
+
+		if r.Method == "POST" {
+			var product entities.Product
+
+			idString := r.FormValue("id")
+			id, err := strconv.Atoi(idString)
+			if err != nil {
+				panic(err)
+			}
+
+			categoryId, err := strconv.Atoi(r.FormValue("category_id"))
+			if err != nil {
+				panic(err)
+			}
+
+			stock, err := strconv.Atoi(r.FormValue("stock"))
+			if err != nil {
+				panic(err)
+			}
+
+			product.Name = r.FormValue("name")
+			product.Category.Id = uint(categoryId)
+			product.Stock = int64(stock)
+			product.Description = r.FormValue("description")
+			product.UpdatedAt = time.Now()
+
+			if ok := productmodel.Update(id, product); !ok {
+				http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+				return
+			}
+
+			http.Redirect(w, r, "/products", http.StatusSeeOther)
+		}
+	}
 
 func Delete(w http.ResponseWriter, r *http.Request) {
+	idString := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := productmodel.Delete(id); err != nil {
+		panic(err)
+	}
+
+	http.Redirect(w, r, "/products", http.StatusSeeOther)
 
 }
